4d75bdb666112116708fdb65e0633775fefd27c1
Animesh Gandhi <itsgandhisince2k1@gmail.com>
2025-01-29 00:48:00 +0530

    done

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -6,9 +6,10 @@ function App() {
 
   return (
     <div className="w-screen h-screen duration-100" style={{backgroundColor:color}}>
+                  <h1 className="text-3xl text-white font-bold text-center">{color} background</h1>
+
     
           <div className="fixed flex flex-wrap bottom-10 justify-center inset-x-0 px-2">
-              <h1 className="text-3xl text-white font-bold mb-80">{color} background</h1>
               <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
                 <button onClick={()=>setColor("violet")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"violet"}}> Violet</button>
                 <button onClick={()=>setColor("indigo")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"indigo"}}> Indigo</button>
